fce28aa2519df156bf42a7e5d3ba829085ab1fde
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-04 00:13:41 +0300

    Last commit

--- React-hooks/src/components/UseEffect.jsx
+++ React-hooks/src/components/UseEffect.jsx
@@ -4,13 +4,18 @@ import axios from "axios";
 function UseEffect() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButton, setIdFromButton] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButton(id);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
       .then((res) => setPost(res.data))
       .catch((err) => console.log(err));
-  }, [id]);
+  }, [idFromButton]);
 
   return (
     <div>
@@ -21,6 +26,7 @@ function UseEffect() {
           setId(e.target.value);
         }}
       />
+      <button onClick={handleClick}>Fetch post title</button>
       <div>{post.title}</div>
     </div>
   );
